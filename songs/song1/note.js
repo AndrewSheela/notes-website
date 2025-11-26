16b946a1f3121ded44b4db335b7b73ee19bf624b
AndrewSheela <josepath.andrew@gmail.com>
2025-11-26 13:57:05 +0530

    Add files via upload
    Added folders and structure

---
+++ songs/song1/note.js
@@ -0,0 +1,5 @@
+const notesData = [
+    { note:"G", trans:"En", tamil:"என்", chord:"Gm7" },
+    { note:"A", trans:"nai", tamil:"னை", chord:"" },
+    { note:"", trans:"-", tamil:"-", chord:"" }
+];
